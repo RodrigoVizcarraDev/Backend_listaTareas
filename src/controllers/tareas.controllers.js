2a4d7cc2165b824833437380ec717405722b5802
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-14 19:41:00 -0300

    Agregada la funcion delete y editar tarea en la ruta

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -43,3 +43,36 @@ export const obtenerTarea = async (req, res) => {
         })
     }
 }
+
+export const borrarTarea = async (req, res) => {
+    try {
+        await Tarea.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            mensaje: "El producto fue eliminado correctamente"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error no se pudo eliminar el producto",
+        })
+    }
+}
+
+export const editarTarea = async (req, res) => {
+    try {
+        const idTarea = req.params.id
+        console.log(idTarea);
+    
+        await Tarea.findByIdAndUpdate(idTarea, req.body);
+    
+        res.status(200).json({
+            mensaje: "El producto fue editado correctamente"
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            mensaje: "Error al editar el producto"
+        })
+    }
+} 
--- src/routes/tarea.routes.js
+++ src/routes/tarea.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { obtenerListaTareas, crearTarea, obtenerTarea } from "../controllers/tareas.controllers";
+import { obtenerListaTareas, crearTarea, obtenerTarea, borrarTarea, editarTarea } from "../controllers/tareas.controllers";
 const router = Router();
 
 
 router.route("/tareas").get(obtenerListaTareas).post(crearTarea)
-router.route("/tareas/:id").get(obtenerTarea)
+router.route("/tareas/:id").get(obtenerTarea).delete(borrarTarea).put(editarTarea)
 
 export default router;
